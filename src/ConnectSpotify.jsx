0a3f06dd008140c118e619a831744dd8df7de415
Daixuan <huodx@ucla.edu>
2023-03-16 21:33:08 -0700

    minimal working version

---
+++ src/ConnectSpotify.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { generateSpotifyLoginURL } from './SpotifyAPI';
+
+async function _connect() {
+    const spotifyURL = await generateSpotifyLoginURL()
+    window.location.href = spotifyURL
+}
+
+function ConnectSpotify() {
+    return (
+        <div>
+            <h1>Connect to Spotify</h1>
+            <button onClick={() => _connect()}>Get Started!</button>
+        </div>)
+}
+
+
+export default ConnectSpotify
